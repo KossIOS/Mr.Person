f84998e92ececa7b2527b75e59536eaf88baf311
Koss <102085029+KossIOS@users.noreply.github.com>
2022-07-04 08:24:12 +0200

    Initial Commit

---
+++ Mr.Person/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mr.Person
+//
+//  Created by Konstantyn Koroban on 04/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
